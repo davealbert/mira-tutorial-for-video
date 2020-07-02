9bd36c0ebd9e6cdbf3bced84993e0c214ea47187
Dave Albert <dave@one15digital.com>
2020-07-02 14:56:22 +0100

    fixes to typos for first  and mira test

--- infra/mira-tut-01-stack.ts
+++ infra/mira-tut-01-stack.ts
@@ -1,6 +1,17 @@
 import { MiraStack } from 'mira'
 import { Construct } from '@aws-cdk/core'
 
+
+import { Queue } from '@aws-cdk/aws-sqs'
+import { Function as LambdaFunction, Runtime, Code } from '@aws-cdk/aws-lambda'
+import { LambdaFunction as LambdaFunctionTarget } from '@aws-cdk/aws-events-targets'
+import { Schedule, Rule } from '@aws-cdk/aws-events'
+
+const AWS_REGION = 'eu-west-3'
+const BASE_NAME = 'davealbert-MiraTutorial01'
+const SQS_BASE_NAME = `${BASE_NAME}SQS`
+const LAMBDA_ASSET_1 = '../../lambda-1/lambda-1.zip'
+
 export default class PlGithubEtlStack extends MiraStack {
   constructor(parent: Construct) {
     super(parent, PlGithubEtlStack.name, {
@@ -9,7 +20,11 @@ export default class PlGithubEtlStack extends MiraStack {
 
 
     // SQS-1
-
+    const sqs1 = new Queue(this, `${SQS_BASE_NAME}1`, {
+      contentBasedDeduplication: true,
+      fifo: true,
+      queueName: `${SQS_BASE_NAME}1.fifo`
+    })
 
     // Lambda-1 -- CRON Job to create SQS-1 message to kick off process
 
