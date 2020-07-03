1729237828c416a19125aef900742a1efb6ee6fd
Dave Albert <dave@one15digital.com>
2020-07-03 07:02:41 +0100

    SQS-1 && Lambda-1 working

--- infra/mira-tut-01-stack.ts
+++ infra/mira-tut-01-stack.ts
@@ -10,7 +10,7 @@ import { Schedule, Rule } from '@aws-cdk/aws-events'
 const AWS_REGION = 'eu-west-3'
 const BASE_NAME = 'davealbert-MiraTutorial01'
 const SQS_BASE_NAME = `${BASE_NAME}SQS`
-const LAMBDA_ASSET_1 = '../../lambda-1/lambda-1.zip'
+const LAMBDA_ASSET_1 = '../lambda-1/lambda-1.zip'
 
 export default class PlGithubEtlStack extends MiraStack {
   constructor(parent: Construct) {
@@ -27,7 +27,22 @@ export default class PlGithubEtlStack extends MiraStack {
     })
 
     // Lambda-1 -- CRON Job to create SQS-1 message to kick off process
+    const lambda1 = new LambdaFunction(this, `${BASE_NAME}Lambda1Handler`, {
+      runtime: Runtime.NODEJS_12_X,
+      code: Code.fromAsset(LAMBDA_ASSET_1),
+      handler: 'index.handler',
+      environment: {
+        SQS_QUEUE_URL: sqs1.queueUrl,
+        REGION: AWS_REGION
+      }
+    })
 
+    const expression = Schedule.expression('cron(0/10 * ? * * *)')
+    const cron = new Rule(this, `${BASE_NAME}CronForLambda2`, {
+      schedule: expression
+    })
+    cron.addTarget(new LambdaFunctionTarget(lambda1))
+    sqs1.grantSendMessages(lambda1)
 
     // S3
 
